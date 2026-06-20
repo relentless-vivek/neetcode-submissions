28b0bbe38768f27036e27bc624d52916a39cb98d
vivek mishra <vivek117257@gmail.com>
2026-06-20 19:35:44 +0530

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> unique = new HashSet<>();
+        for(int i:nums) {
+            if(unique.contains(i)) {
+                return true;
+            }
+            unique.add(i);
+        }
+        return false;
+    }
+}
